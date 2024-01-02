b85cca5d4c4e2a5bd7d1c6cf340cd03802bb8b09
koenvanmeijeren <koenvanmeijeren@develto.nl>
2024-01-02 20:55:27 +0100

    chore: :construction: Improve documentation display

--- tests/Pest.php
+++ tests/Pest.php
@@ -3,16 +3,18 @@
 /**
  * @file
  * The configuration for Pest.
- *
- * |--------------------------------------------------------------------------
- * | Test Case
- * |--------------------------------------------------------------------------
- * |
- * | The closure you provide to your test functions is always bound to a
- * | specific PHPUnit test case class. By default, that class is
- * | "PHPUnit\Framework\TestCase". Of course, you may need to change it using
- * | the "uses()" function to bind a different classes or traits.
- * | E.g. uses(Tests\TestCase::class)->in('Feature');.
+ */
+
+/*
+|--------------------------------------------------------------------------
+| Test Case
+|--------------------------------------------------------------------------
+|
+| The closure you provide to your test functions is always bound to a
+| specific PHPUnit test case class. By default, that class is
+| "PHPUnit\Framework\TestCase". Of course, you may need to change it using
+| the "uses()" function to bind a different classes or traits.
+| E.g. uses(Tests\TestCase::class)->in('Feature');.
  */
 
 /*
