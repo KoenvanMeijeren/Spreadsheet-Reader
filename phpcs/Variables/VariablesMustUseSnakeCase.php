c77a8f07b2d7ec736f73032e0963e3537b7a0bd2
koenvanmeijeren <koenvanmeijeren@develto.nl>
2024-01-03 13:51:50 +0100

    chore: :construction: Fix coding standards

--- phpcs/Variables/VariablesMustUseSnakeCase.php
+++ phpcs/Variables/VariablesMustUseSnakeCase.php
@@ -2,26 +2,34 @@
 
 namespace KoenVanMeijeren\phpcs\Variables;
 
-use PHP_CodeSniffer\Sniffs\Sniff;
 use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
 
-final class VariablesMustUseSnakeCase implements Sniff
-{
-    public function register(): array
-    {
-        return [T_VARIABLE];
+/**
+ * Provides a PHPCS sniff to check that variable names are in snake_case format.
+ */
+final class VariablesMustUseSnakeCase implements Sniff {
+
+  /**
+   * {@inheritDoc}
+   */
+  public function register(): array {
+    return [T_VARIABLE];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  public function process(File $phpcsFile, $stackPtr): void {
+    $tokens = $phpcsFile->getTokens();
+    $varName = $tokens[$stackPtr]['content'];
+
+    // Check if variable name is in snake_case format.
+    if (!preg_match('/^[a-z_][a-z0-9_]*$/', $varName)) {
+      $error = 'Variable name "%s" is not in snake_case format';
+      $data = [$varName];
+      $phpcsFile->addError($error, $stackPtr, 'NotSnakeCase', $data);
     }
+  }
 
-    public function process(File $phpcsFile, $stackPtr): void
-    {
-        $tokens = $phpcsFile->getTokens();
-        $varName = $tokens[$stackPtr]['content'];
-
-        // Check if variable name is in snake_case format
-        if (!preg_match('/^[a-z_][a-z0-9_]*$/', $varName)) {
-            $error = 'Variable name "%s" is not in snake_case format';
-            $data = array($varName);
-            $phpcsFile->addError($error, $stackPtr, 'NotSnakeCase', $data);
-        }
-    }
 }
